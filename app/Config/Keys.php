3a87d2ecab12ad4ff45f15224737542549b929a9
fiankasepman <fianet341@gmail.com>
2025-12-24 10:40:34 +0700

    ignore config

---
+++ app/Config/Keys.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dhiva\Core;
+
+class Keys
+{
+    public static $password = "oczkncHUgqfT5zM8";
+    public static $salt = "cDsflcVFo17L7dNF";
+    public static $iv = "6Se1PwV0JC3AQoZ6";
+    public static $Payload_Key = "j6asnS4Ir9";
+    public static $Payload_IV = "6aeFPBTM8d";
+    public static $iterations = 2;
+    public static $keyLength = 2;
+    public static $route = "izN2pvUmhVViN1FP";
+    public static $JWT_KEY = "o0CV1UPTlF5ydCdJmkXA";
+}
+
+            
